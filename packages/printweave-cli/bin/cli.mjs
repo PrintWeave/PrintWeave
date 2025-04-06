37759269afaf3a24a6bc87dcd41de1efecfde093
Mathijs Markvoort <75422017+Gamermathijs@users.noreply.github.com>
2025-04-06 14:52:29 +0200

    fix update logic

--- packages/printweave-cli/bin/cli.mjs
+++ packages/printweave-cli/bin/cli.mjs
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import {getPathPackage} from './paths.js';
 import inquirer from 'inquirer';
-import User from "@printweave/api/dist/models/user.model.js";
+import {User} from "@printweave/models";
 import dotenv from 'dotenv';
 import * as fs from "node:fs";
 import * as os from "node:os";
@@ -28,8 +28,13 @@ program
     .command('start')
     .description('Start the frontend and API server')
     .option('-m, --method <method>', 'Method to start the server, default is node, available methods: node, forever', 'node')
+    .option('-f, --force', 'Force update without prompt')
     .action(async (options) => {
-        checkUpdates("@printweave/cli")
+        if (options.force) {
+            console.log('Force update option is enabled, skipping prompt');
+        } else {
+            await checkUpdates("@printweave/cli")
+        }
 
         const method = options.method || 'node';
         if (methodIsInstalled(method)) {
--- packages/printweave-cli/bin/update.mjs
+++ packages/printweave-cli/bin/update.mjs
@@ -3,14 +3,17 @@ import util from 'node:util';
 import semver from 'semver';
 import prompts from 'prompts';
 import path from 'node:path';
+import {createRequire} from "module";
 const execAsync = util.promisify(exec);
+
+const require = createRequire(import.meta.url)
 
 async function getInstalledPackageVersion(packageName) {
     try {
         // Attempt to resolve the package relative to the current working directory.
-        const packagePath = path.join(process.cwd(), 'node_modules', packageName, 'package.json');
-        const packageJson = await import(`file://${packagePath}`);  // Use file:// for local imports
-        return packageJson.default.version;
+        // const packagePath = path.join(process.cwd(), 'node_modules', packageName, 'package.json');
+        const packageJson = await require("../package.json");
+        return packageJson.version;
     } catch (error) {
         console.error(`Error getting installed version of ${packageName}:`, error);
         return null;
@@ -19,35 +22,30 @@ async function getInstalledPackageVersion(packageName) {
 
 async function getLatestPackageVersion(packageName) {
     try {
-        const { stdout } = await execAsync(`npm view ${packageName} version`);
-        return stdout.trim();
+        const { stdout } = await execAsync(`npm view ${packageName} dist-tags --json`);
+        const distTags = JSON.parse(stdout);
+        return distTags;
     } catch (error) {
         console.error(`Error getting latest version of ${packageName}:`, error);
         return null;
     }
 }
 
-async function shouldUpdatePackage(currentVersion, latestVersion) {
-    if (!currentVersion || !latestVersion) {
-        return false;
-    }
-    return semver.lt(currentVersion, latestVersion);
-}
-
 async function promptForUpdate(packageName) {
     const response = await prompts({
         type: 'confirm',
         name: 'shouldUpdate',
-        message: `An older version of ${packageName} is installed. Update to the latest version?`,
+        message: `An older version of ${packageName} is installed. Update to the latest version? Use flag --force to skip this prompt.`,
         initial: true,
     });
     return response.shouldUpdate;
 }
 
-async function updatePackage(packageName) {
+async function updatePackage(packageName, tag) {
     try {
-        console.log(`Updating ${packageName} to the latest version...`);
-        const { stdout, stderr } = await execAsync(`npm install ${packageName}@latest`);
+        console.log(`Updating ${packageName} to the latest version with tag ${tag}...`);
+        const { stdout, stderr } = await execAsync(`npm install ${packageName}@${tag}`);
+        console.log(stdout);
         console.log(stdout);
         if (stderr) {
             console.error(stderr);
@@ -58,17 +56,18 @@ async function updatePackage(packageName) {
     }
 }
 
-async function checkUpdates(packageName) {
+export async function checkUpdates(packageName) {
     const currentVersion = await getInstalledPackageVersion(packageName);
-    const latestVersion = await getLatestPackageVersion(packageName);
+    const distTags = await getLatestPackageVersion(packageName);
+    const isAheadOfLatest = semver.lt(currentVersion, distTags['latest']);
+    const currentTag = isAheadOfLatest ? 'latest' : 'next';
 
-    console.log(`Current ${packageName} version: ${currentVersion}`);
-    console.log(`Latest ${packageName} version: ${latestVersion}`);
-
-    if (await shouldUpdatePackage(currentVersion, latestVersion)) {
+    console.log(`Current ${packageName} version: ${currentVersion} (${currentTag})`);
+    console.log(`Latest ${packageName} version: ${distTags[currentTag]} (${currentTag})`);
+    if (semver.lt(currentVersion, distTags[currentTag])) {
         const shouldUpdate = await promptForUpdate(packageName);
         if (shouldUpdate) {
-            await updatePackage(packageName);
+            await updatePackage(packageName, currentTag);
         } else {
             console.log('Update cancelled.');
         }
